0265e1d4e4fcbd956a054d085a93d2dadc02da28
Ivan Baidiuk <ivan.baidiuk@ning.com>
2021-10-23 23:45:00 +0300

    add calculator

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
+const myFunction = function (input) {
+	console.log('input changed')
+	document.getElementById('weight-range').value = input.value
+	input.value = input.value + 'кг'
+}
+
+const myFunction2 = function (input) {
+	console.log(input.value)
+	document.getElementById('weight').value = input.value + 'кг'
+}
+
 window.addEventListener("load", function() {
 	console.log("It woerks!")
 });
+
